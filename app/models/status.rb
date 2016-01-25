8b17473d8daa94b0f0bee35a8d02e127510440e6
vil <casinadie256@hotmail.com>
2016-01-25 07:51:09 -0500

    finalizada etapa de pruebas

--- app/models/status.rb
+++ app/models/status.rb
@@ -4,8 +4,6 @@ class Status < ActiveRecord::Base
   belongs_to :user
 
 
-validates :contenido, presence: true
-                      lenght: { minimum: 2 }
 
 validates :user_id, presence: true                      
 
